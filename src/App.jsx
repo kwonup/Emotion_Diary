b0ccb671700c61aa78bac752f3cce06de187a8b5
kwon youngwoo <kwonyoungwoo@kwonui-MacBookAir.local>
2026-01-23 17:32:50 +0900

    feat: 공통컴포넌트 구성

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,40 @@ import Diary from "./pages/Diary";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Notfound from "./pages/NotFound";
-import Button from "./components/button";
-
+import Button from "./components/Button";
+import Header from "./components/Header";
 //1. "/" : 모든 일기를 조회하는 Home 페이지
 //2. "/new" : 새로운 일기를 작성하는 New 페이지
 //3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
   return (
     <>
-      <Button />
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+      <Button
+        text={"123"}
+        type={"DEFAULT"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!!");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!!");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!!");
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
